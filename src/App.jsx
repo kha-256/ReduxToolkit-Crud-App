fdc660f1871413e9ca0ebf3f5bba9fe3f44c0f68
khadija <khadijanoorhashmi@gmail.com>
2023-06-06 18:57:29 +0500

    Navbar changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import UserDetails from './components/UserDetails'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<>
+<Navbar/>
+<UserDetails/>
+</>
+  )
+}
+
+export default App
