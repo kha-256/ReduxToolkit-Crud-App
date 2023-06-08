f6668c49c9dcf875b9b2ee1550b3a43f501eecf3
khadija <khadijanoorhashmi@gmail.com>
2023-06-08 22:16:41 +0500

    connecting react and redux

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,11 @@
+// index.jsx
 import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "./slice/UserSlice";
+import userSlice from "./slice/UserSlice";
 
-const store= configureStore({
-    reducer:{
-        users:userSlice,
-    }
-})
+const store = configureStore({
+  reducer: {
+    users: userSlice
+  }
+});
 
 export default store;
