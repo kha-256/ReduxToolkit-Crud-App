848711100dc65461f0f259f050cb8eee47475ce3
khadija <khadijanoorhashmi@gmail.com>
2023-06-11 22:51:21 +0500

    adding delete user functionality

--- src/components/DispalyUsers.jsx
+++ src/components/DispalyUsers.jsx
@@ -1,20 +1,28 @@
 import { useSelector } from "react-redux";
 import styled from "styled-components";
-
-
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useDispatch } from "react-redux";
+import { removeUser } from "../store/slice/UserSlice";
 
 const DisplayUsers = () => {
 
+  const dispatch= useDispatch();
     const data= useSelector((state)=>{
         return state.users
     })
 
-    console.log(data)
+    const deleteUser=(payload)=>{
+      dispatch(removeUser(payload))
+    }
   return (
     <Wrapper>
      {data.map((user,id)=>{
         return <li>
             {user}
+            <button className="btn-delete" onClick={()=> deleteUser(id)}>
+            <DeleteForeverIcon sx={{color:'red', fontSize:20, cursor:"pointer"}} />
+            </button>
+            
         </li>
      })}
     </Wrapper>
@@ -33,7 +41,17 @@ const Wrapper = styled.section`
   &:first-child{
       border-top: 1px solid #eee
   }
+  &:last-child{
+      border-bottom: none
+  }
+},
+
+.btn-delete{
+    background-color: transparent;
+    border: none;
+   
 }
+
 `;
 
 export default DisplayUsers;
