403410ac68b57a7d8f156d0d9556cd4c84069df6
khadija <khadijanoorhashmi@gmail.com>
2023-06-14 22:01:45 +0500

    error removal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,9 @@ import styled from "styled-components";
 const Navbar = () => {
   return (
     <Wrapper>
-      <div>Welcome, to Education Hub</div>
       <h1>Admin Table</h1>
 
       <section>
-        <div>One Destination for complete Web Development</div>
         <nav>
           <ul className="menuItems">
             <li>
